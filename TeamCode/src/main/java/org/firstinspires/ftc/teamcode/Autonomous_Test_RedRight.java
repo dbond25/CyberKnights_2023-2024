0ea0ce9986b8138c250876d66c79cf874310b98a
Gingebreadman <mbakerafa@gmail.com>
2024-01-19 18:47:03 -0700

    Updated RedRight

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous_Test_RedRight.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous_Test_RedRight.java
@@ -118,7 +118,7 @@ public class Autonomous_Test_RedRight extends LinearOpMode
 
 
     private static final boolean USE_WEBCAM = true;  // Set true to use a webcam, or false for a phone camera
-    private static final int DESIRED_TAG_ID = 1;     // Choose the tag you want to approach or set to -1 for ANY tag.
+    private static final int DESIRED_TAG_ID = 4;     // Choose the tag you want to approach or set to -1 for ANY tag.
     private VisionPortal visionPortal;               // Used to manage the video source.
     private AprilTagProcessor aprilTag;              // Used for managing the AprilTag detection process.
     private AprilTagDetection desiredTag = null;     // Used to hold the data for a detected AprilTag
